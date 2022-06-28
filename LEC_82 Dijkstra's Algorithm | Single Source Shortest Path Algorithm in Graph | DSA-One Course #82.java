effbdb6cc2f4fb6b74be69b4cb5ab0bf51178099
Punit pawar <70194383+SkyrunnerOO7@users.noreply.github.com>
2022-06-28 15:27:46 +0530

    Create LEC_82 Dijkstra's Algorithm | Single Source Shortest Path Algorithm in Graph | DSA-One Course #82.java

---
+++ LEC_82 Dijkstra's Algorithm | Single Source Shortest Path Algorithm in Graph | DSA-One Course #82.java
@@ -0,0 +1,65 @@
+
+//PAIR CALSS FOR STORING VERTEX AND WEIGHT
+    class Pair implements Comparable<Pair>{
+        int wt;
+        int v;
+        
+        Pair(int v, int wt)
+        {
+            this.v = v;
+            this.wt = wt;
+        }
+        
+        public int compareTo(Pair that)
+        {
+            return this.wt-that.wt;
+        }
+    }
+
+class Solution
+{
+    //Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
+    {
+        // Write your code here
+        boolean vis[] = new boolean[V];
+        
+        PriorityQueue<Pair> q = new PriorityQueue<>();
+        
+        q.add(new Pair(S, 0));
+        
+        int ans[] = new int[V];
+        Arrays.fill(ans, Integer.MAX_VALUE);
+        
+        ans[S] = 0;
+        
+        while(q.size() != 0)
+        {
+            Pair cur = q.remove();
+            
+            int u = cur.v;
+            if(vis[u]){continue;}
+            
+            vis[u] = true;
+            
+            ArrayList<ArrayList<Integer>> neighbors = adj.get(u);
+            
+            for(ArrayList<Integer> list : neighbors)
+            {
+                int vertex = list.get(0);
+                int wt = list.get(1);
+                
+                if(ans[vertex] > ans[u]+wt)
+                {
+                    ans[vertex] = ans[u]+wt;
+                    q.add(new Pair(vertex, ans[vertex]));
+                }
+            }
+        }
+        
+        return ans;
+        
+        
+    }
+}
